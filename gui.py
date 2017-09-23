8da484506e2057df9d20348f3a60c52821b7e8dd
MF <notsure@compuserv.com>
2017-09-23 15:17:54 -0500

    made a search button!

--- gui.py
+++ gui.py
@@ -5,5 +5,13 @@ class GUI:
     def __init__(self):
         self.main_window = tkinter.Tk()
         self.main_window.wm_title("FourPlusOne's Encyclopedia")
-
+        self.search_frame = tkinter.Frame(self.main_window)
+        self.search_button = tkinter.Button(self.search_frame, text = "Search:",
+                                            command = runSearch)
+        self.search_button.pack()
+        self.search_frame.pack()
         tkinter.mainloop()
+
+
+def runSearch():
+    print("searching...")
