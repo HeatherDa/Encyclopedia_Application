eb53c7b904240cfd05cc8e9f793e27ffbc640399
MF <notsure@compuserv.com>
2017-09-23 22:15:02 -0500

    added checkboxes

--- gui.py
+++ gui.py
@@ -1,23 +1,41 @@
-import tkinter
+from tkinter import *
 
 
 class GUI:
     def __init__(self):
-        self.mainWindow = tkinter.Tk()
+        # Makes main window widget and gives it a title.
+        self.mainWindow = Tk()
         self.mainWindow.wm_title("FourPlusOne's Encyclopedia")
+
+        # Retrieves the current screen's width and height.
+        # Fractions of these values are used to determine
+        # the size of the main window.
         windowWidth = self.mainWindow.winfo_screenwidth() / 4
         windowHeight = self.mainWindow.winfo_screenheight() * .6
         self.mainWindow.geometry("%dx%d" % (windowWidth, windowHeight))
 
-        self.checkboxFrame = tkinter.Frame(self.mainWindow)
-        self.searchFrame = tkinter.Frame(self.mainWindow)
-        self.searchButton = tkinter.Button(self.searchFrame, text ="Search:",
+        # Sets up the checkboxes in their own frame.
+        self.checkboxFrame = Frame(self.mainWindow)
+        self.cboxValue1 = IntVar()
+        self.cboxValue2 = IntVar()
+        self.cboxValue3 = IntVar()
+
+        self.checkbox1 = Checkbutton(self.checkboxFrame, text = "Wikipedia", variable = self.cboxValue1)
+        self.checkbox2 = Checkbutton(self.checkboxFrame, text="not", variable=self.cboxValue2)
+        self.checkbox3 = Checkbutton(self.checkboxFrame, text="sure", variable=self.cboxValue3)
+        self.checkbox1.pack()
+        self.checkbox2.pack()
+        self.checkbox3.pack()
+        self.checkboxFrame.pack()
+
+        self.searchFrame = Frame(self.mainWindow)
+        self.searchButton = Button(self.searchFrame, text ="Search:",
                                            command = runSearch)
         self.searchButton.pack()
         self.searchFrame.pack()
 
 
-        tkinter.mainloop()
+        mainloop()
 
 
 def runSearch():
