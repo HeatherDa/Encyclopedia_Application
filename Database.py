bf5b5672a78b9a6c08624a6d60db8df58882744b
DarthVader <edwin.melendez@students.ipr.edu>
2017-09-30 12:59:46 -0500

    Added user table to database class, needs a few more query methods

--- Database.py
+++ Database.py
@@ -4,9 +4,17 @@ import sqlite3
 sqlite_file = '/Users/DarthVader/Desktop/Encyclopedia_Application/history.sqlite'
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
-table_name = 'History'
+
+table_user = 'Users'
+userID = 'user_ID'
+user_name = 'user_name'
+password = 'user_password'
+first = 'user_first_name'
+last = 'user_last_name'
+table_search = 'History'
 col1 = 'search_ID'
 col2 = 'search_item'
+col3 = 'user_ID'
 
 # Database class
 
@@ -14,11 +22,21 @@ col2 = 'search_item'
 class Database:
 
     # create database table
-    def create_table(self):
+    def create_search_table(self):
 
-        c.execute('CREATE TABLE TABLE_NAME(col1 INTEGER PRIMARY KEY NULL, col2 TEXT)')
+        c.execute('CREATE TABLE TABLE_SEARCH(col1 INTEGER PRIMARY KEY NULL, col2 VARCHAR,'
+                  ' col3 INTEGER FOREIGN KEY REFERENCES TABLE_USER(userID))')
 
         conn.commit()
+
+    # create database table
+    def create_user_table(self):
+            c.execute('CREATE TABLE TABLE_USER(userID INTEGER PRIMARY KEY NULL,user_name VARCHAR,'
+                      ' password VARCHAR, first TEXT, last Text)')
+
+            conn.commit()
+
+
 
     # plug in user input from textbox to save item to database
     def add_result(self, item):
@@ -26,17 +44,27 @@ class Database:
         search_item = item
 
         try:
-            c.execute('INSERT INTO TABLE_NAME(col2) VALUES (?)', search_item)
+            c.execute('INSERT INTO TABLE_SEARCH(col2) VALUES (?)', search_item)
 
         except sqlite3.IntegrityError:
             print('Error with PRIMARY KEY')
 
         conn.commit()
 
-    # returns rows of search items stored in database, use in the combobox for search history
-    def display_all(self):
+    def add_user(self, uname, pw, fname, lname):
 
-        c.execute('SELECT * FROM TABLE_NAME')
+        try:
+            c.execute('INSERT INTO TABLE_USER(user_name, password, first, last) VALUES (?, ?, ?, ?)'
+                      , (uname, pw, fname, lname))
+        except sqlite3.IntegrityError:
+            print('Error with PRIMARY KEY')
+
+        conn.commit()
+
+    # returns rows of search items stored in database, use in the combobox for search history
+    def display_all_history(self):
+
+        c.execute('SELECT * FROM TABLE_SEARCH')
 
         all_rows = c.fetchall()
 
@@ -45,11 +73,17 @@ class Database:
 
         conn.commit()
 
+# todo: create a join that gets specific user history depending
+
+    # def display_user_history(self, user_name):
+
+      #  c.execute('SELECT ')
+
     # deletes the table, for testing purpose
-    def delete_table(self):
+    def delete_tables(self):
 
-        c.execute('DROP TABLE TABLE_NAME')
-
+        c.execute('DROP TABLE TABLE_SEARCH')
+        c.execute('DROP TABLE TABLE_USER')
         conn.commit()
 
     # closes database connection
