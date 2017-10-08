2f570922cc51513796282e7bfc20e5a6f965c9ed
MF <notsure@compuserv.com>
2017-10-08 18:17:12 -0500

    created login page

--- flaskapp.py
+++ flaskapp.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
    return render_template('webpage.html')
 
+@app.route('/login')
+def loginRoute():
+    return render_template('login.html')
+
 if __name__ == '__main__':
    app.run(debug = True)
