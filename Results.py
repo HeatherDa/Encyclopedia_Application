42f1db87e2391fdb92ec8b9d3a8957a766428374
maihall13 <maiahall1396@gmail.com>
2017-10-17 15:33:36 -0500

    edit results more. Currently testing in the results.py Full website is not working since i have to update it still

--- Results.py
+++ Results.py
@@ -1,10 +1,23 @@
 import StarWarsAPI
 import WikipediaAPI
 
+def getWikipedia(word):
+    wikiList = WikipediaAPI.getSearchResults("hello")
+    wikiDict = dict.fromkeys(wikiList)
+    print(wikiDict)
+    for key, value in wikiDict.items():
+        try:
+            word = WikipediaAPI.getPage(key)
+            wikiDict[key] = [word.pageid, word.url, word.content]
+        except:
+            wikiDict[key] = "delete"
+    return(wikiDict)
+
+
 def getList(word):
     wikiList = WikipediaAPI.getSearchResults(word)
     starList = StarWarsAPI.StarWarsAPI(word).getData()
     final_list = set(starList + wikiList)
     return (final_list)
 
-print (getList("dark"))
+print (getWikipedia("hello"))
--- WikipediaAPI.py
+++ WikipediaAPI.py
@@ -5,6 +5,7 @@ def getNumberOfResults(query):
     #query = input(str("Enter your search"))
     results = wikipedia.search(query,suggestion=True)
     return (len(results[0]))
+
 # returns a list of suggestions for the search if a word is spelled wrong
 def getSuggestions(query):
     suggestions = wikipedia.suggest(query)
@@ -23,13 +24,19 @@ def getTitle(query):
 def getSummary(query):
     summary = wikipedia.summary(query, sentences=1)
     return summary
+
 #returns a list of image links for the search
 def getImages(query):
     page = wikipedia.page(query)
     images = page.images
     return images
+
 #Returns the URL of the page
 def getURL(query):
     page = wikipedia.page(query)
     url = page.url
     return url
+
+def getPage(query):
+    page = wikipedia.page(query)
+    return page
