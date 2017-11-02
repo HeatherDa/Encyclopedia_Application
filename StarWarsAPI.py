875a6b04c1a0ee273af4ae65e7c183457566ed7d
user <user@useremail.com>
2017-11-02 18:24:47 -0500

    final

--- StarWarsAPI.py
+++ StarWarsAPI.py
@@ -14,48 +14,58 @@ class StarWarsAPI:
     def getData(self):
         list = []
         formatedUrl = requests.get("{0}/{1}/?search={2}".format(self.baseURL, "people", self.search))
-        json_data =json.loads(formatedUrl.content)
-        results = json_data["results"]
-        if len(results) == 0:
-            list.append("NA")
+        try:
+            json_data =json.loads(formatedUrl.content)
+            results = json_data["results"]
+
+
+
+            if len(results) == 0:
+                list.append("NA")
+                return list
+            else:
+                ships = ""
+                films = ""
+                specie = ""
+                world = ""
+                list.append(results[0]["name"])
+                list.append(results[0]["gender"])
+                list.append(results[0]["birth_year"])
+                if len(results[0]["starships"]) > 0:
+                    for ship in results[0]["starships"]:
+                        ships += self.getStarShip(ship) +", "
+                    ships = ships[:-2]
+                    list.append(ships)
+                else:
+                    ships = "NA"
+                    list.append(ships)
+                if len(results[0]["films"]) > 0:
+                    for film in results[0]["films"]:
+                        films += self.getFilms(film) +", "
+                    films = films[:-2]
+                    list.append(films)
+                else:
+                    films = "NA"
+                    list.append(films)
+                if len(results[0]["species"]) > 0:
+                    for species in results[0]["species"]:
+                        specie += self.getSpecies(species) + ", "
+                    specie = specie[:-2]
+                    list.append(specie)
+                else:
+                    specie = "NA"
+                    list.append(specie)
+                if results[0]["homeworld"] != 0:
+                    world += self.getHomeWorld(results[0]["homeworld"]) + ", "
+                world = world[:-2]
+                list.append(world)
+        except (Exception):
+            list.append("Too many requests today; please try again tomorrow!")
             return list
-        else:
-            ships = ""
-            films = ""
-            specie = ""
-            world = ""
-            list.append(results[0]["name"])
-            list.append(results[0]["gender"])
-            list.append(results[0]["birth_year"])
-            if len(results[0]["starships"]) > 0:
-                for ship in results[0]["starships"]:
-                    ships += self.getStarShip(ship) +", "
-                ships = ships[:-2]
-                list.append(ships)
-            else:
-                ships = "NA"
-                list.append(ships)
-            if len(results[0]["films"]) > 0:
-                for film in results[0]["films"]:
-                    films += self.getFilms(film) +", "
-                films = films[:-2]
-                list.append(films)
-            else:
-                films = "NA"
-                list.append(films)
-            if len(results[0]["species"]) > 0:
-                for species in results[0]["species"]:
-                    specie += self.getSpecies(species) + ", "
-                specie = specie[:-2]
-                list.append(specie)
-            else:
-                specie = "NA"
-                list.append(specie)
-            if results[0]["homeworld"] != 0:
-                world += self.getHomeWorld(results[0]["homeworld"]) + ", "
-            world = world[:-2]
-            list.append(world)
-        return list
+        finally:
+            return list
+
+
 
 #Below are helper methods for the getData method, please ignore
     def getStarShip(self,ships):
